a7d6fd34b6e0d1032a378aef962a975c4a1a31ab
Franziska Adam <franziskacarolineadam@gmail.com>
2023-11-30 11:32:59 +0100

    Solve Create API

---
+++ pages/api/random-character.js
@@ -0,0 +1,14 @@
+import Chance from "chance";
+
+const chance = new Chance();
+
+export default function handler(request, response) {
+  const character = {
+    firstName: chance.first(),
+    lastName: chance.last(),
+    age: chance.age(),
+    profession: chance.profession(),
+  };
+
+  return response.status(200).json({ character });
+}
